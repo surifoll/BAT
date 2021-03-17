fde97676529b25f5634323d81780453b07a5bcd6
SFEHINT <suraj.fehintola@lafargeholcim.com>
2021-03-17 14:27:37 +0100

    [Suraj]

---
+++ BAT.WebApi/Migrations/20210317132659_Init.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BAT.WebApi.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsMale = table.Column<bool>(type: "bit", nullable: false),
+                    IsOnWhatsApp = table.Column<bool>(type: "bit", nullable: false),
+                    IsOnTelegram = table.Column<bool>(type: "bit", nullable: false),
+                    Dob = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PreferredBrand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AlternativeBrand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BrandSampled = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BrandPurchased = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GameName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Score = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PriceWon = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diys", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Diys");
+        }
+    }
+}
